4cd14e192fd39bbc0e86c70af95146c52c38245b
S <s>
2012-09-11 00:25:43 +0200

    {"type":"TI", "runs":"2"}

---
+++ BeeperTotingKarel.java
@@ -0,0 +1,26 @@
+/*
+ * File: BeeperTotingKarel.java
+ * ----------------------------
+ * The BeeperTotingKarel class extends the basic Karel class
+ * so that Karel picks up a beeper from 1st Street and then
+ * carries that beeper to the center of a ledge on 2nd Street.
+ */
+
+import stanford.karel.*;
+
+public class BeeperTotingKarel extends Karel {
+	public void run() {
+		move();
+		pickBeeper();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		putBeeper();
+		move();
+	} 
+}

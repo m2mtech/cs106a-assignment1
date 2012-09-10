8efe70e8a8416d2111ef47d4a0e9860723b936b1
S <s>
2012-09-10 23:53:22 +0200

    {"type":"TI", "runs":"0"}

---
+++ CheckerboardKarel.java
@@ -0,0 +1,23 @@
+/*
+ * File: CheckerboardKarel.java
+ * ----------------------------
+ * When you finish writing it, the CheckerboardKarel class should draw
+ * a checkerboard using beepers, as described in Assignment 1.  You
+ * should make sure that your program works for all of the sample
+ * worlds supplied in the starter folder.
+ */
+
+import stanford.karel.*;
+
+/*
+ * Name: 
+ * Section Leader: 
+ */
+
+public class CheckerboardKarel extends SuperKarel {
+
+	public void run() {
+		// You fill in this part
+	}
+
+}

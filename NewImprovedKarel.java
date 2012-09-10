ea1f42535dfff4af693294fd74fdc6799cc58310
S <s>
2012-09-11 00:30:49 +0200

    {"type":"TI", "runs":"0"}

---
+++ NewImprovedKarel.java
@@ -0,0 +1,26 @@
+/*
+ * File: NewImprovedKarel.java
+ * ---------------------------
+ * The NewImprovedKarel class extends the basic Karel class
+ * so that any subclasses have access to the turnRight and
+ * turnAround methods.  It does not define any run method
+ * of its own.
+ */
+import stanford.karel.*;
+public class NewImprovedKarel extends Karel {
+	/**
+	 * Turns Karel 90 degrees to the right.
+	 */
+	public void turnRight() {
+		turnLeft();
+		turnLeft();
+		turnLeft();
+	}
+	/**
+	 * Turns Karel around 180 degrees.
+	 */
+	public void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
+}

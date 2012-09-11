0fa06c630587af1e121be40fcbfee2f7f7147042
S <s>
2012-09-11 09:48:46 +0200

    {"type":"TI", "runs":"0"}

--- RoadRepairKarel.java
+++ RoadRepairKarel.java
@@ -1,23 +1,33 @@
 /* /*
  * File: RoadRepairKarel.java
  * --------------------------
- * The RoadRepairKarel class fills a series of regularly
- * spaced potholes until it reaches the end of the roadway.
+ * This version of the RoadRepairKarel class fills an
+ * arbitrary sequence of potholes in a roadway.
  */
 import stanford.karel.*;
 public class RoadRepairKarel extends SuperKarel {
 	public void run() {
 		while (frontIsClear()) {
+			checkForPothole();
 			move();
+		}
+		checkForPothole();
+	}
+	/**
+	 * Checks for a pothole immediately beneath Karel's current
+	 * looking for a wall to the right.  If a pothole exists,
+	 * Karel calls fillPothole to repair it.
+	 */
+	private void checkForPothole() {
+		if (rightIsClear()) {
 			fillPothole();
-			move(); 
 		}
 	}
 	/**
-	 * Fills the hole beneath Karel's current position by
-	 * placing a beeper in the hole.  For this method to
+	 * Fills the pothole beneath Karel's current position by
+	 * placing a beeper on that corner.  For this method to
 	 * work correctly, Karel must be facing east immediately
-	 * above the hole.  When execution is complete, Karel
+	 * above the pothole.  When execution is complete, Karel
 	 * will have returned to the same square and will again
 	 * be facing east.  This version of fillPothole checks to
 	 * see if there is already a beeper present before putting

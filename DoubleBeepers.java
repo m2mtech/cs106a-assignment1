9005f10378eab1b09e8543c9e6b649d8d379bf96
S <s>
2012-09-11 10:12:30 +0200

    {"type":"TI", "runs":"1"}

---
+++ DoubleBeepers.java
@@ -0,0 +1,17 @@
+/*
+ * File: DoubleBeepers.java
+ * ------------------------
+ * Karel doubles the number of beepers on the corner directly
+ * in front of him in the world. He then returns to his * original position/orientation.
+ */
+import stanford.karel.SuperKarel;
+
+public class DoubleBeepers extends SuperKarel {
+
+	public void run() {
+		move();
+		DoubleBeepersInPile();
+		moveBackward();
+	}
+	
+	

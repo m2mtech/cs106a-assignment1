5f865733f6a39fcecaa4ed32fac949752d14f308
m2m tech <tech@m2m.at>
2012-09-14 18:39:47 +0200

    assignment #1 task #3

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -15,8 +15,80 @@ import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
 
+	/*
+	 * Pre-condition:  Facing East at bottom left corner
+	 * Post-condition: Facing North at top right or left corner
+	 */
 	public void run() {
-		// You fill in this part
+		oddRow();
+		faceNorth();
+		while (frontIsClear()) {
+			if (beepersPresent()) {
+				move();
+				faceRow();
+				evenRow();
+			} else {
+				move();
+				faceRow();
+				oddRow();
+			}
+			faceNorth();
+		}
+	}
+	
+	/*
+	 * Planting every second field, starting with the first one
+	 * Pre-condition:  Facing into direction of row
+	 * Post-condition: Last position of row
+	 */
+	private void oddRow() {
+		putBeeper();
+		while(frontIsClear()) {
+			move();
+			if (frontIsClear()) {
+				move();
+				putBeeper();
+			}
+ 		}
+	}
+
+	/*
+	 * Planting every second field, starting with the second one
+	 * Pre-condition:  Facing into direction of row
+	 * Post-condition: Last position of row
+	 */
+	private void evenRow() {
+		while(frontIsClear()) {
+			move();
+			putBeeper();
+			if (frontIsClear()) {
+				move();
+			}
+ 		}
+	}
+
+	/*
+	 * Pre-condition:  Facing north
+	 * Post-condition: Facing into direction of row
+	 */
+	private void faceRow() {
+		if (rightIsBlocked()) {
+			turnLeft();
+		} else if (leftIsBlocked()) {
+			turnRight();
+		} 		
+	}	
+
+	/*
+	 * Pre-condition:  Facing east or west
+	 * Post-condition: Facing north
+	 */
+	private void faceNorth() {
+		if (facingEast()) {
+			turnLeft();
+		} else if (facingWest()) {
+			turnRight();
+		} 		
 	}
 
 }
